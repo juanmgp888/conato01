5f5dab8d80ed71a5a05854575a149d08955c6545
Juanma <Juanma888@gmail.com>
2024-11-17 14:52:42 +0100

    Más métodos en String01

--- src/main/java/es/jmg/ArrayMulti.java
+++ src/main/java/es/jmg/ArrayMulti.java
@@ -4,7 +4,9 @@ public class ArrayMulti {
         
         int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
 
-      for (int i = 0; i < myNumbers.length; i++) {
+      for (int i = 0; i < myNumbers.length; i++) { 
+        // length no lleva () pq es un atributo, no un método
+        // Con un String hubiese sido diferente, con ().
           for (int j = 0; j < myNumbers[i].length; j++) {
             System.out.println(myNumbers[i][j]);
           }
--- src/main/java/es/jmg/String01.java
+++ src/main/java/es/jmg/String01.java
@@ -2,14 +2,45 @@ package es.jmg;
 public class String01{
     public static void main(String[] args){
 
-    // Reverse a string
-    String origen = "aeiou";
-    String destino = "";
+        // Reverse a string
+        String origen = "aeiou";
+        String destino = "";
+  
+        for (int i=0;i<origen.length();i++){
+            destino += origen.charAt(origen.length()-1-i);
+            System.out.println(origen.charAt(origen.length()-1-i));        
+            }
+        System.out.println(destino);
+        
 
-    for (int i=0;i<origen.length();i++){
-        destino += origen.charAt(origen.length()-1-i);
-        System.out.println(origen.charAt(origen.length()-1-i));        
+        // Qué posición de la cadena ocupa el carácter 'i'
+        for (int i=0;i<origen.length();i++){ // Al ser un String, el length es un método y lleva ()
+            if (origen.charAt(i) == 'i'){  // == de comparación, no poner = de asignación !!
+                System.out.println("La 'i' ocupa (empezando por cero) la posición número "+i+" en la cadena "+origen);
+            }
         }
-    System.out.println(destino);
+
+        // Extrae una subcadena que no contenga ni el caracter inicial ni el final
+        System.out.println(origen.substring(1, origen.length()-1));
+
+        // Compara si dos cadenas son iguales siendo case sensitive
+        String cadenaComparacion = "AEIOU";
+        String cadenaComparacion2 = "aeiou";
+
+        if (origen.equals(cadenaComparacion)){
+            System.out.println("Sí, la cadena "+origen+ " es idéntica a "+cadenaComparacion);
+        }
+        else { System.out.println("No, la cadena "+origen+ " NO es idéntica a "+cadenaComparacion);};
+        
+        if (origen.equals(cadenaComparacion2)){
+            System.out.println("Sí, la cadena "+origen+ " es idéntica a "+cadenaComparacion2);
+        }
+        else { System.out.println("No, la cadena "+origen+ " NO es idéntica a "+cadenaComparacion2);};
+
+        // Compara sin case sensitive
+        if (origen.equalsIgnoreCase(cadenaComparacion)){
+            System.out.println("Sí, las vocales de la cadena "+origen+ " son las mismas que las de la cadena: "+cadenaComparacion);
+        }
+        else { System.out.println("No, las vocales de la cadena "+origen+ " NO son las mismas que las de la cadena: "+cadenaComparacion);};
     }
 }
